e6a6023c87ceba01e146f6f94d9c29d3d01caf59
gkharish <gkharish@laas.fr>
2015-08-20 01:54:39 +0200

    Report is updated. Datainfo.csv is added.

--- ident_data/jointtestresponses.m
+++ ident_data/jointtestresponses.m
@@ -1,16 +1,23 @@
+%% Program to identify  and simulate the identified transfer function
+% There are several data sets in this folder. Muscles are first initialized with some pressure then a step input is fed to record the response.
+% We gradually increase the initial pressure in muscle1 in different experiments.
+% However, the sum of the pressure in muscle1 and muscle2 is always kept constant to 4.2bar. 
+% The identification is done considering delta P as input and position of the elbow as output. 
+% In every data set, Varname6 is the pressure in muscle1 which is the input and 
+% Varname8 is the output angular position.
+% The Input-output pair is created from Varname6 and Varname8.
+% xlow to Xup is the region of interest for creating input-output data pair for identification. 
+
+step1: Bring a particular dataset to the workspace.
+Step2: Choose the region of interest in plot(Varname8) which covers the transient behavour or see the datainfo.csv
+Step3: Select the different 
+%%
 s = tf('s');
 
-%pneumaticarm_sys = exp(-s*0.23)*K_gain*wn^2/(s^2 + 2*damping_ratio*wn*s + wn^2) - 0.09/2.2
-pneumaticarm_sys = tfdelpinitpres9%exp(-s*0.11)*K_gain*wn^2/(s^2 + 2*damping_ratio*wn*s + wn^2);
-%plot(2*step(pneumaticarm_sys))
 
+xlow = 4700
+xup =  5500;
 
-% for j = 1:1:300
-% U(j) = -0.02;
-% end
-%xrange = 1000+1;
-xlow = 8850
-xup =  9500;
 % T= 0:0.01:(xrange-1)*0.01;
 % for i = 1:1:xrange
 %     if (i > xlow && i <= xup)                  % 928 && i <= 18370 for the file initpres2
@@ -32,6 +39,10 @@ O = outputdata;
 figure(1)
 %bode(Ze)
 %% Simulate the system
+%pneumaticarm_sys = exp(-s*0.23)*K_gain*wn^2/(s^2 + 2*damping_ratio*wn*s + wn^2) - 0.09/2.2
+pneumaticarm_sys = tfdelpinitpres7%exp(-s*0.11)*K_gain*wn^2/(s^2 + 2*damping_ratio*wn*s + wn^2);
+%plot(2*step(pneumaticarm_sys))
+
 ss_sys = ss(pneumaticarm_sys)
 X0 = [ VarName8(xlow);0;0];
 T= 0:0.01:(xup -xlow)*0.01;
@@ -60,3 +71,22 @@ ylabel('Angular position elbow (rad)');
 % subplot(212), plot(VarName1(xrange_low:xrange_up), U, 'r');
 % %inputdataidentsinewave = U';
 %outputdataidentsinewave = VarName8(1:xrange);
+
+% [A1, B1] = ssdata(tfdelpinitpres1)
+% [A2, B2] = ssdata(tfdelpinitpres2)
+% [A41, B41] = ssdata(tfdelpinitpres41)
+% [A6, B6] = ssdata(tfdelpinitpres6)
+% [A7, B7] = ssdata(tfdelpinitpres7)
+% [A8, B8] = ssdata(tfdelpinitpres8)
+% [A9, B9] = ssdata(tfdelpinitpres9)
+% [A10, B10] = ssdata(tfdelpinitpres10)
+% statespace1 = [A1, B1];
+% statespace2 = [A2, B2];
+% statespace41 = [A41, B41]
+% statespace6 = [A6, B6];
+% statespace7 = [A7, B7];
+% statespace8 = [A8, B8]; 
+% statespace9 = [A9, B9];
+% statespace10 = [A10, B10]; 
+% [A1, B1] = ssdata(tfdelpinitpres1)
+% [A1, B1] = ssdata(tfdelpinitpres1)
